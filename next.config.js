16e3953530657b5804cdc7f873268939e56a963a
Mitchell Lee <mitchell.lee@hsw.com.au>
2021-06-24 12:54:00 +1000

    Typo'd when updated bathPath

--- next.config.js
+++ next.config.js
@@ -24,7 +24,7 @@ module.exports = (phase, { defaultConfig }) => {
         ...defaultConfig,
         poweredByHeader: false,
         reactStrictMode: true, // Good Practice apparently
-        basePath: '/products-releases',
+        basePath: '/product-releases',
         headers: headers,
     };
 
